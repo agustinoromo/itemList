b212e850c1abc43729245a6b042b86aaf1807a82
Agustin <agustinoromo@gmail.com>
2020-04-22 19:29:22 -0700

    Item Model and subclass modeling for array of multiple child types

---
+++ ItemList/Model/Item.swift
@@ -0,0 +1,120 @@
+//
+//  Item.swift
+//  ItemList
+//
+//  Created by Agustin Mendoza Romo on 4/22/20.
+//  Copyright © 2020 Agustin Mendoza Romo. All rights reserved.
+//
+
+import Foundation
+
+class Item: Decodable {
+    var name: String
+    var type: String
+    var description: String
+
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case type
+        case description
+    }
+}
+
+class Car: Item {
+    var maker: String
+    var year: Int
+
+    private enum CodingKeys: String, CodingKey {
+        case maker, year
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.maker = try container.decode(String.self, forKey: .maker)
+        self.year = try container.decode(Int.self, forKey: .year)
+        try super.init(from: decoder)
+    }
+}
+
+class Phone: Item {
+    var model: String
+    var operatingSystem: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case model
+        case operatingSystem = "os"
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.model = try container.decode(String.self, forKey: .model)
+        self.operatingSystem = try container.decode(String.self, forKey: .operatingSystem)
+        try super.init(from: decoder)
+    }
+}
+
+class Book: Item {
+    var author: String
+    var yearPublished: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case author
+        case yearPublised = "year_published"
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.author = try container.decode(String.self, forKey: .author)
+        self.yearPublished = try container.decode(Int.self, forKey: .yearPublised)
+        try super.init(from: decoder)
+    }
+}
+
+struct Items: Decodable {
+    let items: [Item]
+
+    enum ItemsKey: CodingKey {
+        case items
+    }
+
+    enum ItemTypeKey: CodingKey {
+        case type
+    }
+
+    enum ItemTypes: String, Decodable {
+        case book = "Book"
+        case orangeJuice = "orange_juice"
+        case car = "Car"
+        case phone = "Phone"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ItemsKey.self)
+        var itemsArrayForType = try container.nestedUnkeyedContainer(forKey: ItemsKey.items)
+        var items = [Item]()
+
+        var itemsArray = itemsArrayForType
+        while(!itemsArrayForType.isAtEnd)
+        {
+            let item = try itemsArrayForType.nestedContainer(keyedBy: ItemTypeKey.self)
+            let type = try item.decode(ItemTypes.self, forKey: ItemTypeKey.type)
+            switch type {
+            case .orangeJuice:
+                print("found drink")
+                items.append(try itemsArray.decode(Item.self))
+            case .car:
+                print("found beer")
+                items.append(try itemsArray.decode(Car.self))
+            case .phone:
+                print("found phone")
+                items.append(try itemsArray.decode(Phone.self))
+            case .book:
+                print("found book")
+                items.append(try itemsArray.decode(Book.self))
+            }
+        }
+        self.items = items
+    }
+}
+
+
--- ItemList/ViewController.swift
+++ ItemList/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var itemList = """
+    {
+        "items": [
+            {
+                "name": "Sprint",
+                "type": "Book",
+                "description": "All natural",
+                "author": "Jake Knapp",
+                "year_published": 2010
+            },
+            {
+                "name": "Carlsbad Dealer",
+                "type": "Car",
+                "description": "An alcoholic beverage, best drunk on fridays after work",
+                "maker": "Honda",
+                "year": 2015
+            },
+            {
+                "name": "Angela's phone",
+                "type": "Phone",
+                "description": "Best phone of 2018",
+                "model": "Samsung Galaxy Note",
+                "os": "Android"
+            }
+        ]
+    }
+    """
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let jsonDecoder = JSONDecoder()
+        do {
+            let results = try jsonDecoder.decode(Items.self, from:itemList.data(using: .utf8)!)
+            for result in results.items {
+                print(result.description)
+                if let car = result as? Car {
+                    print(car.maker, car.name, car.description)
+                }
+                if let phone = result as? Phone {
+                    print(phone.model, phone.name, phone.description, phone.operatingSystem)
+                }
+                if let book = result as? Book {
+                    print(book.author, book.name, book.description, book.yearPublished)
+                }
+            }
+        } catch {
+            print("caught: \(error)")
+        }
     }
 
 
+    
+    
 }
 

bdd40e40c352585232f9d81e96d3faa0c6fc96cd
Agustin <agustinoromo@gmail.com>
2020-04-22 20:57:01 -0700

    Migrate mock response to JSON file

--- ItemList/ViewController.swift
+++ ItemList/ViewController.swift
@@ -10,86 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var itemList = """
-    {
-        "items": [
-            {
-                "name": "Sprint",
-                "type": "Book",
-                "description": "All natural",
-                "author": "Jake Knapp",
-                "year_published": 2010
-            },
-            {
-                "name": "Accord",
-                "type": "Car",
-                "description": "",
-                "maker": "Honda",
-                "year": 2015
-            },
-            {
-                "name": "Angela's phone",
-                "type": "Phone",
-                "description": "Best phone of 2018",
-                "model": "Samsung Galaxy Note",
-                "os": "Android"
-            },{
-                "name": "Masked Prey",
-                "type": "Book",
-                "description": "The 30th book in the Prey series. Washington politicians ask Lucas ",
-                "author": "John Sanford",
-                "year_published": 2020
-            },
-            {
-                "name": "Michaels's phone",
-                "type": "Phone",
-                "description": "The best phone for people who don't want to spend a bundle",
-                "model": "iPhone 11",
-                "os": "iOS"
-            },
-            {
-                "name": "Corolla",
-                "type": "Car",
-                "description": "",
-                "maker": "Toyota",
-                "year": 2010
-            },
-            {
-                "name": "Eric's phone",
-                "type": "Phone",
-                "description": "The OnePlus 8 Pro is a fantastic value because it packs so many features into a phone that costs less",
-                "model": "OnePlus 8 Pro",
-                "os": "Android"
-            }
-        ]
-    }
-    """
-
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        parseJSON()
         
-        let jsonDecoder = JSONDecoder()
-        do {
-            let results = try jsonDecoder.decode(Items.self, from:itemList.data(using: .utf8)!)
-            for result in results.items {
-                print(result.description)
-                if let car = result as? Car {
-                    print(car.maker, car.name, car.description)
-                }
-                if let phone = result as? Phone {
-                    print(phone.model, phone.name, phone.description, phone.operatingSystem)
-                }
-                if let book = result as? Book {
-                    print(book.author, book.name, book.description, book.yearPublished)
-                }
-            }
-        } catch {
-            print("Error: \(error)")
-        }
     }
 
-
-    
+    private func parseJSON() {
+        if let path = Bundle.main.path(forResource: "MockItemResponse", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonDecoder = JSONDecoder()
+                let results = try jsonDecoder.decode(Items.self, from:data)
+                for result in results.items {
+                    print(result.description)
+                    if let car = result as? Car {
+                        print(car.maker, car.name, car.description)
+                    }
+                    if let phone = result as? Phone {
+                        print(phone.model, phone.name, phone.description, phone.operatingSystem)
+                    }
+                    if let book = result as? Book {
+                        print(book.author, book.name, book.description, book.yearPublished)
+                    }
+                }
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
     
 }
 
